9172246b7f83d27d7f209ec6108cc8acf36edeab
hammonjj <james@thegruppetto.com>
2023-06-15 14:33:10 -0600

    More service worker updates

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,29 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App.tsx'
 import './index.css'
+import { registerSW } from 'virtual:pwa-register'
+
+declare global {
+  interface Window {
+    updateApp: () => void;
+  }
+}
+
+// Register the service worker
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // show a prompt to user
+    console.log('A new version is available. Do you want to update?');
+  },
+  onOfflineReady() {
+    // show a ready to work offline to user
+    console.log('The app is ready to work offline.');
+  },
+})
+
+// Attach the updateSW function to some global scope that
+// you can access from anywhere in your app. For example, to the window object.
+window.updateApp = updateSW;
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-pwa/client" />
+
+declare module 'virtual:pwa-register' {
+  const RegisterSW: (options?: {
+    immediate?: boolean
+    onNeedRefresh?: () => void
+    onOfflineReady?: () => void
+  }) => () => void
+  export default RegisterSW
+}
